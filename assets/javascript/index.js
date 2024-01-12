e7dfc08b72d66dd8291bb50d60626c12f1a27dce
Onur Esin <onuresn034@gmail.com>
2024-01-12 20:13:31 +0300

    SSS kısmı tamamlandı

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -32,3 +32,29 @@ document.addEventListener("DOMContentLoaded", function() {
       selectedContent.style.display = "block";
     }
   }
+//**************************************************************/
+
+
+//SSS Algoritması
+document.addEventListener("DOMContentLoaded", function() {
+    const accContents = document.querySelectorAll(".acc-content");
+  
+    accContents.forEach(function(content) {
+      const question = content.querySelector(".question");
+  
+      question.addEventListener("click", function() {
+        accContents.forEach(function(innerContent) {
+          if (innerContent !== content) {
+            innerContent.classList.remove("active");
+            innerContent.querySelector(".answer").style.display = "none";
+          }
+        });
+  
+        content.classList.toggle("active");
+  
+        const answer = content.querySelector(".answer");
+        answer.style.display = answer.style.display === "block" ? "none" : "block";
+      });
+    });
+  });
+  
