fd6426ed1cbe0315f799b928012ed44c2f71280c
Onur Esin <onuresn034@gmail.com>
2024-01-12 18:20:08 +0300

    slider links kısmı tamamlandı

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -14,3 +14,21 @@ document.querySelectorAll(".modal").forEach((n) => {
     });
 });
 //**************************************************************/
+
+//Slider Algoritması
+document.addEventListener("DOMContentLoaded", function() {
+    showContent(1);
+  });
+  
+  function showContent(index) {
+    let contents = document.getElementsByClassName("content");
+    for (let i = 0; i < contents.length; i++) {
+      contents[i].style.display = "none";
+    }
+  
+    let contentId = "content" + index;
+    let selectedContent = document.getElementById(contentId);
+    if (selectedContent) {
+      selectedContent.style.display = "block";
+    }
+  }
