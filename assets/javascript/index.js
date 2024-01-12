dd141da711c99d2988354a4c196d8aeacab8f9d6
Onur Esin <onuresn034@gmail.com>
2024-01-12 21:33:50 +0300

    mail check kısmı bitti

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function() {
 //**************************************************************/
 
 
-//SSS Algoritması
+//SSS Algoritması Accordion
 document.addEventListener("DOMContentLoaded", function() {
     const accContents = document.querySelectorAll(".acc-content");
   
@@ -49,12 +49,64 @@ document.addEventListener("DOMContentLoaded", function() {
             innerContent.querySelector(".answer").style.display = "none";
           }
         });
-  
         content.classList.toggle("active");
-  
         const answer = content.querySelector(".answer");
         answer.style.display = answer.style.display === "block" ? "none" : "block";
       });
     });
   });
-  
+  //**************************************************************/
+  // Email Validate Function Code
+
+  function validateEmail() {
+    const emailInput = document.getElementById('emailInput');
+    const errorIcon = document.getElementById('errorIcon');
+    const errorMessage = document.querySelector('.error-message');
+    const mailBtn = document.querySelector('.mail-btn');
+    const emailborder = document.getElementById('emailborder');
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailPattern.test(emailInput.value)) {
+        errorMessage.style.display = 'none';
+        alert('Kaydınız başarılı, artık haberler ve güncellemelerimizi görebileceksiniz!');
+        emailborder.style.border = '2px solid transparent';
+        errorIcon.style.display = 'none';
+
+        // Geçerli email girişi yaptıktan sonra input içeriğini temizle
+        emailInput.value = '';
+    } else {
+        emailborder.style.border = '2px solid var(--WarmRed)';
+        errorIcon.style.display = 'inline';
+        errorMessage.style.display = 'block';
+        mailBtn.classList.remove('active');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const emailInput = document.getElementById('emailInput');
+    const errorIcon = document.getElementById('errorIcon');
+    const errorMessage = document.querySelector('.error-message');
+    const mailBtn = document.querySelector('.mail-btn');
+    const emailborder = document.getElementById('emailborder');
+
+    emailInput.addEventListener('input', function() {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (emailPattern.test(emailInput.value)) {
+            // Geçerli bir email adresi girdiğinde
+            errorIcon.style.display = 'none';
+            errorMessage.style.display = 'none';
+            emailborder.style.border = '0';
+            mailBtn.classList.add('active');
+        } else {
+            // Geçersiz bir email adresi girdiğinde
+            errorIcon.style.display = 'inline';
+            errorMessage.style.display = 'block';
+            emailborder.style.border = '2px solid var(--WarmRed)';
+            mailBtn.classList.remove('active');
+        }
+    });
+});
+
+
