96100732439918340bb41a80ac066cc45e7f1ea4
Onur Esin <onuresn034@gmail.com>
2024-01-13 18:28:34 +0300

    slider responsive tamamlandı

--- assets/javascript/slider.js
+++ assets/javascript/slider.js
@@ -1,29 +1,38 @@
 //Slider Algoritması
 document.addEventListener("DOMContentLoaded", function() {
-    showContent(1);
+  showContent(1); // Sayfa yüklendiğinde gelecek içeriği göstermek için 
+  window.addEventListener("resize", function() {
+      showContent(1); // Pencere boyutu değiştiğinde içeriğin column veya row olmasını anlık değiştirmek için
   });
-  
-  function showContent(index) {
-    let contents = document.getElementsByClassName("content");
-    for (let i = 0; i < contents.length; i++) {
-      contents[i].style.display = "none";
-    }
-  
-    let contentId = "content" + index;
-    let selectedContent = document.getElementById(contentId);
-    if (selectedContent) {
-      selectedContent.style.display = "block";
-    }
+});
 
-    let selectedDiv = document.querySelector('.slider-links .title:nth-child(' + index + ')');
-    let allImages = document.querySelectorAll('.red-stck');
-    for (let j = 0; j < allImages.length; j++) {
+function showContent(index) {
+  let contents = document.getElementsByClassName("content");
+  let windowWidth = window.innerWidth;
+
+  for (let i = 0; i < contents.length; i++) {
+      contents[i].style.display = "none";
+  }
+
+  let contentId = "content" + index;
+  let selectedContent = document.getElementById(contentId);
+
+  if (selectedContent) {
+      selectedContent.style.display = "flex";
+      selectedContent.style.flexDirection = windowWidth >= 1440 ? "row" : "column";
+  }
+
+  let selectedDiv = document.querySelector('.slider-links .title:nth-child(' + index + ')');
+  let allImages = document.querySelectorAll('.red-stck');
+
+  for (let j = 0; j < allImages.length; j++) {
       allImages[j].style.display = "none";
   }
 
   let selectedImage = selectedDiv.querySelector('.red-stck');
+
   if (selectedImage) {
-    selectedImage.style.display = "block";
-}
+      selectedImage.style.display = "flex";
+  }
 }
 
